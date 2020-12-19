39fae907bee2b1d471b82c38d2e884f85d7a8f1f
teodutu <teodor.dutu@gmail.com>
2020-12-19 22:11:46 +0200

    [Tema 2][~50p] Gasire ineficienta a solutiei corecte

--- Teme/Tema2/gas.py
+++ Teme/Tema2/gas.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
-from typing import List, Tuple
+from heapq import heappop, heappush
+from typing import Callable, List, Tuple
 
 from levels import *
-from util import check_state, state_eq, p, prints
+from util import check_state, state_eq, p, prints, iswin
 
 """
 #######################################
@@ -118,8 +119,104 @@ def press(color: str, state: List[List[str or int]]) -> List[List[str or int]]:
     return _move_square(deepcopy(state), _get_colour_pos(state, color), None)
 
 
-def solve(initial_state: List[List[str or int]]) -> Tuple[List[str], List[List[List[str or int]]], int]:
-    return ["", [initial_state], 0]
+def _find_colours(state: List[List[str or int]]):
+    return [elem[COLOR] for elem in state if elem[TYPE] == SQUARE]
+
+
+def _solve_bfs(
+    initial_state: List[List[str or int]]
+) -> Tuple[List[str], List[List[List[str or int]]], int]:
+    colours = _find_colours(initial_state)
+    num_explored = 0
+
+    q = [([], initial_state)]
+
+    while q:
+        num_explored += 1
+        cmds, crt_state = q.pop(0)
+
+        if iswin(crt_state):
+            break
+
+        for colour in colours:
+            q.append((cmds + [colour], press(colour, crt_state)))
+
+    explored = [initial_state]
+    for cmd in cmds:
+        initial_state = press(cmd, initial_state)
+        explored.append(initial_state)
+
+    return cmds, explored, num_explored
+
+
+def _get_squares(state: List[List[str or int]]) -> List[List[str or int]]:
+    return [elem for elem in state if elem[TYPE] == SQUARE]
+
+
+def _get_goal_for_colour(
+    state: List[List[str or int]],
+    colour: str
+) -> Tuple[int, int]:
+    for elem in state:
+        if elem[COLOR] == colour:
+            return elem[X], elem[Y]
+
+
+def manhattan(state: List[List[str or int]]) -> int:
+    dist = 0
+
+    for square in _get_squares(state):
+        x, y = _get_goal_for_colour(state, square[COLOR])
+        dist += abs(square[X] - x) + abs(square[Y] - y)
+
+    return dist
+
+
+def _solve_astar(
+    initial_state: List[List[str or int]],
+    h
+) -> Tuple[List[str], List[List[List[str or int]]], int]:
+    frontier = []
+    heappush(frontier, (0 + h(initial_state), initial_state))
+    discovered = {str(initial_state): (0, [])}
+    colours = _find_colours(initial_state)
+    num_explored = 1
+
+
+    while frontier:
+        crt_state = list(heappop(frontier)[1])
+        crt_g, cmds = discovered[str(crt_state)]
+
+        if iswin(crt_state):
+            break
+
+        for colour in colours:
+            next_g = crt_g + 1
+            next_s = press(colour, crt_state)
+
+            next_s_str = str(next_s)
+
+            if (
+                next_s_str not in discovered
+                or next_g < discovered[next_s_str][0]
+            ):
+                discovered[next_s_str] = (next_g, cmds + [colour])
+                heappush(frontier, (next_g + h(next_s), next_s))
+                num_explored += 1
+
+    explored = [initial_state]
+    for cmd in cmds:
+        initial_state = press(cmd, initial_state)
+        explored.append(initial_state)
+
+    return cmds, explored, num_explored
+
+
+def solve(
+    initial_state: List[List[str or int]]
+) -> Tuple[List[str], List[List[List[str or int]]], int]:
+    # return _solve_bfs(initial_state)
+    return _solve_astar(initial_state, manhattan)
 
 
 def play(initstate: List[List[str or int]], plan: List[str], small: bool=False):
